3069ec44983f16173981afb197b783015b3f1448
JEFIRO <150871603+JEFIRO@users.noreply.github.com>
2025-03-08 17:01:13 -0300

    Refactor PermissionService with role-based validations
    Added role-based validation to prevent modifying admin accounts for changing status, locking accounts, or altering permissions. Introduced methods for account locking and permission changes, ensuring robust error handling and user feedback.

--- src/main/java/com/SchoolNews/jefiro/br/service/PermissionService.java
+++ src/main/java/com/SchoolNews/jefiro/br/service/PermissionService.java
@@ -1,5 +1,6 @@
 package com.SchoolNews.jefiro.br.service;
 
+import com.SchoolNews.jefiro.br.models.enumm.MemberRole;
 import com.SchoolNews.jefiro.br.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,57 @@ public class PermissionService {
     @Autowired
     private MemberRepository memberRepository;
 
+
     public String chengeUserStatus(String id) {
         var member = memberRepository.findById(id);
-        if (member.isPresent()){
+        if (member.isPresent()) {
             var memberGet = member.get();
+            if (memberGet.getRole() == MemberRole.ADMIN) {
+                return "You can't change an admin status";
+            }
             memberGet.setStatus(!memberGet.getStatus());
             memberRepository.save(memberGet);
-        }else return "User not found";
+        } else return "User not found";
 
         return "User status changed successfully";
     }
 
-    public void deleteUser(String id) {}
+    public String AccountLocked(String id) {
+        var member = memberRepository.findById(id);
+
+        if (member.isEmpty()) {
+            return "User Not found";
+        }
+
+        var memberGet = member.get();
+
+        if (memberGet.getRole() == MemberRole.ADMIN) {
+            return "You can't block an admin account";
+        }
+
+        memberGet.setAccountNotLocked(!memberGet.getAccountNotLocked());
+        memberRepository.save(memberGet);
+
+
+        return "User blockd changed successfully";
+    }
+
+    public String changedPemission(String id, MemberRole role) {
+        var member = memberRepository.findById(id);
+        if (member.isEmpty()) {
+            return "User Not found";
+        }
+
+        var memberGet = member.get();
+
+        if (memberGet.getRole() == MemberRole.ADMIN) {
+            return "You can't change an admin pemission";
+        }
+
+        memberGet.setRole(role);
+        memberRepository.save(memberGet);
+
+        return "User permission changed successfully";
+    }
 
 }
